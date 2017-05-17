29b46c247c4786a49d8903cd61e745914e6c999e
Stanislav Ostrovskiy <stanislavostrovskiy@Stanislavs-MacBook-Pro.local>
2017-05-17 11:42:48 -0400

    initial commit

---
+++ TableViewWithMultipleCellTypes/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TableViewWithMultipleCellTypes
+//
+//  Created by Stanislav Ostrovskiy on 4/25/17.
+//  Copyright © 2017 Stanislav Ostrovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    fileprivate let viewModel = ProfileViewModel()
+    
+    @IBOutlet weak var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView?.dataSource = viewModel
+        
+        tableView?.estimatedRowHeight = 100
+        tableView?.rowHeight = UITableViewAutomaticDimension
+        
+        tableView?.register(AboutCell.nib, forCellReuseIdentifier: AboutCell.identifier)
+        tableView?.register(NamePictureCell.nib, forCellReuseIdentifier: NamePictureCell.identifier)
+        tableView?.register(FriendCell.nib, forCellReuseIdentifier: FriendCell.identifier)
+        tableView?.register(AttributeCell.nib, forCellReuseIdentifier: AttributeCell.identifier)
+        tableView?.register(EmailCell.nib, forCellReuseIdentifier: EmailCell.identifier)
+    }
+}
